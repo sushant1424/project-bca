b663945691bd5b8d23071dcfb3beedee2f78c16e
Sushant Shrestha <sushantshrestha184@gmail.com>
2025-09-05 22:45:02 +0545

    Fix terser build issue by switching to esbuild

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
